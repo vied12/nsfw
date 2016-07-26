d569ae3e84d5e394ea7d2b4b5df6e50beb335370
Edouard <edou4rd@gmail.com>
2016-07-26 20:20:06 +0200

    datavis in station page

--- app/static/station/module.js
+++ app/static/station/module.js
@@ -35,5 +35,126 @@
         });
     }
     angular.module('nsfw')
-    .controller('StationCtrl', StationCtrl);
+    .controller('StationCtrl', StationCtrl)
+    .directive('nsfwDatavis', function() {
+        return {
+            scope: {},
+            link: function(scope, element) {
+                var width = 960,
+                    height = 156,
+                    cellSize = 17; // cell size
+
+                var percent = d3.format('.1%'),
+                    format = d3.time.format('%Y%m%d'),
+                    formatMonth = d3.time.format('%B'),
+                    formatPercent = d3.format('.0%'),
+                    formatNumber = d3.format('.0f');
+
+                var threshold = d3.scale.threshold()
+                    .domain([0, 35, 50])
+                    .range(['#fff68d', '#fff68f', 'rgb(227, 161, 140)', '#d9534f']);
+
+                var svg = d3.select(element.get(0)).selectAll('svg')
+                    .data(d3.range(2013, 2017))
+                    .enter().append('svg')
+                        .attr('width', width)
+                        .attr('height', height)
+                        .attr('class', 'year')
+                        .append('g')
+                            .attr('transform', 'translate(' + ((width - cellSize * 53) / 2) + ',' + (height - cellSize * 7 - 1) + ')');
+
+                svg.append('text')
+                    .attr('transform', 'translate(-6,' + cellSize * 3.5 + ')rotate(-90)')
+                    .style('text-anchor', 'middle')
+                    .text(function(d) { return d; });
+
+                var rect = svg.selectAll('.day')
+                    .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+                  .enter().append('rect')
+                    .attr('class', 'day')
+                    .attr('width', cellSize)
+                    .attr('height', cellSize)
+                    .attr('x', function(d) { return d3.time.weekOfYear(d) * cellSize; })
+                    .attr('y', function(d) { return d.getDay() * cellSize; })
+                    .datum(format);
+
+                rect.append('title')
+                    .text(function(d) { return d; });
+
+                var months = svg.selectAll('.month')
+                    .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+                    .enter();
+                months.append('text')
+                    .attr('style', 'transform: translateY(-5px);')
+                    .attr('class', 'month-label')
+                    .attr('x', function(d) {
+                        var padding = d.getDay() === 0 ? 0 : 1;
+                        return (d3.time.weekOfYear(d) + padding) * cellSize;
+                    })
+                    .text(function(d) { return formatMonth(d); });
+                months.append('path')
+                    .attr('class', 'month')
+                    .attr('d', monthPath);
+
+                d3.json('/static/data.json', function(error, json) {
+                    if (error) throw error;
+                    var data = json.values;
+                    rect.filter(function(d) { return d in data; })
+                    .style('fill', function(d) { return threshold(data[d][0]); })
+                    .select('title')
+                    .text(function(d) {return format.parse(d).toISOString().slice(0, 10) + ': ' + data[d][0] + 'µg/m³'; });
+                });
+
+                function monthPath(t0) {
+                    var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+                        d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
+                        d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
+                    return 'M' + (w0 + 1) * cellSize + ',' + d0 * cellSize +
+                           'H' + w0 * cellSize + 'V' + 7 * cellSize +
+                           'H' + w1 * cellSize + 'V' + (d1 + 1) * cellSize +
+                           'H' + (w1 + 1) * cellSize + 'V' + 0 +
+                           'H' + (w0 + 1) * cellSize + 'Z';
+                }
+
+
+                // A position encoding for the key only.
+                var x = d3.scale.linear()
+                    .domain([0, 70])
+                    .range([0, 270]);
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient('bottom')
+                    .tickSize(13)
+                    .tickValues(threshold.domain());
+
+                var legend = d3.select(element.get(0)).append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+
+                var g = legend.append('g')
+                    .attr('class', 'key')
+                    .attr('transform', 'translate(' + (width - 240) + ',' + height / 2 + ')');
+
+                g.selectAll('rect')
+                    .data(threshold.range().map(function(color) {
+                      var d = threshold.invertExtent(color);
+                      if (d[0] === undefined) d[0] = x.domain()[0];
+                      if (d[1] === undefined) d[1] = x.domain()[1];
+                      return d;
+                    }))
+                  .enter().append('rect')
+                    .attr('height', 8)
+                    .attr('x', function(d) {return x(d[0]); })
+                    .attr('width', function(d) {return x(d[1]) - x(d[0]); })
+                    .style('fill', function(d) { return threshold(d[0]); });
+
+                g.call(xAxis)
+                .append('text')
+                    .attr('class', 'caption')
+                    .attr('y', -6)
+                    .text('Concentration of PM10 pollution in µg/m³');
+
+            }
+        };
+    });
 })();
