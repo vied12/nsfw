ad94cea89fe8178076ba1a92b0689e3bbeef8158
Edouard <edou4rd@gmail.com>
2016-08-01 10:57:10 +0200

    load data for all station

--- app/nsfw/management/commands/all_data.py
+++ app/nsfw/management/commands/all_data.py
@@ -5,20 +5,25 @@ import datetime
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--berlin', help='load only berlin data', action='store_const', const=True)
+
     def handle(self, *args, **kwargs):
         today = datetime.datetime.now().strftime('%Y%m%d')
         for station in Station.objects.all():
-            if station.id.startswith('DEBE'):
-                print(station)
-                pm10_r = requests.get('https://www.umweltbundesamt.de/en/luftdaten/\
+            if kwargs['berlin'] and not station.id.startswith('DEBE'):
+                continue
+            print(station)
+            pm10_r = requests.get('https://www.umweltbundesamt.de/en/luftdaten/\
 data?pollutant=PM1&data_type=1TMW&date=20130101&\
 dateTo={today}&station={station}'.format(today=today, station=station.id))
-                no2 = requests.get('https://www.umweltbundesamt.de/en/luftdaten/\
+            no2 = requests.get('https://www.umweltbundesamt.de/en/luftdaten/\
 data?pollutant=NO2&data_type=1TMAX&date=20130101&\
 dateTo={today}&station={station}'.format(today=today, station=station.id))
-                pm10 = pm10_r.content.decode('utf8')
-                no2 = no2.content.decode('utf8')
-                if pm10 or no2:
-                    station.pm10_data = pm10
-                    station.no2_data = no2
-                    station.save()
+            pm10 = pm10_r.content.decode('utf8')
+            no2 = no2.content.decode('utf8')
+            if pm10 or no2:
+                station.pm10_data = pm10
+                station.no2_data = no2
+                station.save()
