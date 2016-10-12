02a8a9558098dc9cbd408336164071dac84071c8
Edouard <edou4rd@gmail.com>
2016-10-12 12:09:51 +0200

    translate last alerts

--- app/static/alerts/module.js
+++ app/static/alerts/module.js
@@ -9,15 +9,24 @@
             templateUrl: 'static/alerts/last.html',
             controllerAs: 'vm',
             bindToController: true,
-            controller: function() {
+            controller: ['gettextCatalog', 'gettext', function(gettextCatalog, gettext) {
                 var vm = this;
+                var tooltip = gettext('{{ value }}µg/m³ is the limit for {{ kind }} pollution based on WHO recommendations');
+                function tooltipBody(alert) {
+                    return gettextCatalog.getString(tooltip, {
+                            kind: alert.report.kind,
+                            value: vm.limits[alert.report.kind]
+                        }
+                    );
+                }
                 angular.extend(vm, {
                     limits: {
                         PM10: 50,
                         NO2: 200,
-                    }
+                    },
+                    tooltipBody: tooltipBody
                 });
-            }
+            }]
         };
     });
 })();
