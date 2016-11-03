6d2073d2529b0238df82e70b814f5daea90d0ad3
Edouard <edou4rd@gmail.com>
2016-11-03 19:39:05 +0100

    logging for heroku

--- app/settings_heroku.py
+++ app/settings_heroku.py
@@ -16,3 +16,36 @@ EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'nsfw': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('nsfw')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
