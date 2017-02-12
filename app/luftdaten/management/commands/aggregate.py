ae324b6fe891f316e89b61cd2f43f22767c20012
Edouard Richard <edou4rd@gmail.com>
2017-02-12 12:43:17 +0100

    tz

--- app/luftdaten/management/commands/aggregate.py
+++ app/luftdaten/management/commands/aggregate.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from app.luftdaten.models import SensorValue, SensorValueAggregated
-import datetime
+from django.utils import timezone
 
 
 def get_hour(date):
-    return datetime.datetime(date.year, date.month, date.day, date.hour) + datetime.timedelta(hours=1)
+    return timezone.datetime(date.year, date.month, date.day, date.hour) + timezone.timedelta(hours=1)
 
 
 def merge_rows(rows):
